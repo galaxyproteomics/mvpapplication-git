6621ba26025a3e0c927ac9950fa020e9b9cb59ad
Thomas McGowan <mcgo0092@umn.edu>
2019-10-02 10:52:25 -0500

    Clean up during build.sh

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ cp $CONFIG ./$PACKAGE_NAME/config
 
 cat $SOURCE_LOCATION/js/modules/*js > $SOURCE_LOCATION/application_full.js && cat $SOURCE_LOCATION/app.js >> $SOURCE_LOCATION/application_full.js
 uglifyjs $SOURCE_LOCATION/application_full.js --compress --mangle -o $SOURCE_LOCATION/application.js
+rm $SOURCE_LOCATION/application_full.js
 mv $SOURCE_LOCATION/application.js ./$PACKAGE_NAME/static/js
 
 tar -czf ./$PACKAGE_NAME.tar.gz ./$PACKAGE_NAME
